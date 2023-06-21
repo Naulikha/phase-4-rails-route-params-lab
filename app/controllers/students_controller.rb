8bef61aefc94535c683f382c365f18fc8c90ef39
Naulikha <nbiketia@gmail.com>
2023-06-21 06:32:10 +0300

    Completed assignment

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,20 @@
 class StudentsController < ApplicationController
+    def index
+      if params[:name]
+        # Find students whose first or last name matches the query parameter
+        students = Student.where("LOWER(first_name) LIKE LOWER(:query) OR LOWER(last_name) LIKE LOWER(:query)", query: "%#{params[:name]}%")
+      else
+        # Retrieve all students if no query parameter is provided
+        students = Student.all
+      end
+  
+      render json: students
+    end
+  
+    def show
+      student = Student.find(params[:id])
+      render json: student
+    end 
 
-  def index
-    students = Student.all
-    render json: students
-  end
 
 end
